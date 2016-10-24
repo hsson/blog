f57227dd991561450acc6c2755a0bbb1f22fe706
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-10-24 20:22:32 +0200

    Adds basic layout to the frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterializeDirective } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterializeDirective
   ],
   imports: [
     BrowserModule,
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,6 @@
 import './polyfills.ts';
+import 'materialize-css';
+import 'angular2-materialize';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
