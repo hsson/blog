6c490b2e4ab7e20da01cf717ce0a4b6d2a39134e
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-11-01 14:29:34 +0100

    Adds component for editing blog posts

---
+++ frontend/src/app/admin-edit-post/admin-edit-post.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AdminEditPostComponent } from './admin-edit-post.component';
+
+describe('Component: AdminEditPost', () => {
+  it('should create an instance', () => {
+    let component = new AdminEditPostComponent();
+    expect(component).toBeTruthy();
+  });
+});
