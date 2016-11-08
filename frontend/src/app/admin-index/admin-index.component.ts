8a8cbec3f14eb332554bfc4d8fe60158bac21c11
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-11-08 20:07:39 +0100

    Makes the new-post component more modular

--- frontend/src/app/admin-index/admin-index.component.ts
+++ frontend/src/app/admin-index/admin-index.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+import { BlogPost } from '../shared/blogPost';
+import { PostService } from '../post.service';
+import { Router } from '@angular/router';
+
 const COMPONENT_TITLE = "Admin | blog.hakansson.xyz";
 
 @Component({
@@ -10,14 +14,34 @@ const COMPONENT_TITLE = "Admin | blog.hakansson.xyz";
 })
 export class AdminIndexComponent implements OnInit {
 
+  post = new BlogPost();
+
   preview = false;
 
   constructor(
-    private titleService: Title
+    private titleService: Title,
+    private postService: PostService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.titleService.setTitle(COMPONENT_TITLE);
+  }
+
+  createNewPost(): void {
+    if (!this.post.title || !this.post.body) {
+      return;
+    }
+
+    this.postService
+      .newPost(this.post)
+      .then(newPost => this.processPost(newPost));
+  }
+
+   processPost(post: BlogPost): void {
+    console.log(post);
+    let link = ["/post", post.slug];
+    this.router.navigate(link);
   }
 
 }
